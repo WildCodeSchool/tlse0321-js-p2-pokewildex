be383be4cfe50e221ca12e0dae2eb1bd12838933
Chloe MORENO <corentin.prevost@wildcodeschool.com>
2021-04-21 10:38:31 +0200

    added css to searchbar

--- src/Components/PokedexList.jsx
+++ src/Components/PokedexList.jsx
@@ -41,31 +41,38 @@ function PokedexList() {
   if (pokemons) {
     return (
       <>
-        {prevPage && (
+        <section className="pokedex-explorer">
+          {prevPage && (
+            <button
+              className="pokedex-button"
+              type="submit"
+              onClick={() => setCurrPage(prevPage)}
+            >
+              ←
+            </button>
+          )}
+          <input
+            className="explorer"
+            placeholder="searchbar"
+            onChange={(e) => handleSearch(e.target.value)}
+          />
           <button
-            className="pokedex-button"
+            className="explorer"
             type="submit"
-            onClick={() => setCurrPage(prevPage)}
+            onClick={() => handleSubmit()}
           >
-            ←
+            GO
           </button>
-        )}
-        <input
-          placeholder="searchbar"
-          onChange={(e) => handleSearch(e.target.value)}
-        />
-        <button type="submit" onClick={() => handleSubmit()}>
-          GOOOOO
-        </button>
-        {nextPage && (
-          <button
-            className="pokedex-button"
-            type="submit"
-            onClick={() => setCurrPage(nextPage)}
-          >
-            →
-          </button>
-        )}
+          {nextPage && (
+            <button
+              className="pokedex-button"
+              type="submit"
+              onClick={() => setCurrPage(nextPage)}
+            >
+              →
+            </button>
+          )}
+        </section>
         <div className="pokedex-cards">
           {pokemons.map((pokemon) => (
             <PokedexDetails key={pokemon.name} pokemonId={pokemon.name} />
